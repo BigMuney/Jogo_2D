9105790a0f99714ab239961b88b0053b47025a03
BigMuneyMan - Maykon <minhamanhaa@gmail.com>
2026-09-15 19:03:47 -0300

    move update

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -3,10 +3,9 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    public float Velocity;
-    public float MinSpeed;
-    public float MaxSpeed;
-    public float Speed;
+    public float Velocity, MaxSpeedL, MaxSpeedR, Speed;
+    public int JumpPower;
+    public bool CanJump = false;
     public Rigidbody2D RB;
     public BoxCollider2D Collider;
     void Start()
@@ -18,10 +17,23 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") == true )
+        Speed = RB.linearVelocityX;
+        if (Input.GetKeyDown("space") == true && CanJump == true )
         {
-            RB.linearVelocityY = 10f;
-            Debug.Log("space");
+            RB.linearVelocityY = JumpPower;
+            CanJump = false;
+        }
+        if (Input.GetAxis("Horizontal") != 0 && RB.linearVelocityX <= MaxSpeedR && RB.linearVelocityX >= MaxSpeedL)
+        {
+            float HorizontalMove = Input.GetAxis("Horizontal");
+            RB.linearVelocityX = (HorizontalMove * Velocity);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground") == true)
+        {
+            CanJump = true;
         }
     }
 }
